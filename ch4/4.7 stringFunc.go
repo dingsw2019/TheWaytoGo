f094d4d74ee8044b7043de5245bca46e971f4ac5
liupengyu13461 <liupengyu13461@ipalfish.com>
2020-11-10 12:07:56 +0800

    containsAny理解错误，修正

--- ch4/4.7 stringFunc.go
+++ ch4/4.7 stringFunc.go
@@ -1,6 +1,6 @@
 /*
 	strings 和 strconv 包主要函数演示
- */
+*/
 
 package main
 
@@ -16,50 +16,46 @@ func main() {
 	// 1. 前缀和后缀
 	var str string = "This is an example of a string"
 	// 字符串str 是否以 th 开头, 返回布尔型
-	fmt.Printf("%t\n",strings.HasPrefix(str,"Th")) // true
-	fmt.Printf("%t\n",strings.HasPrefix(str,"TH")) // false
+	fmt.Printf("%t\n", strings.HasPrefix(str, "Th")) // true
+	fmt.Printf("%t\n", strings.HasPrefix(str, "TH")) // false
 	// 字符串str 是否以 ing 结尾, 返回布尔型
-	fmt.Printf("%t\n",strings.HasSuffix(str,"ing")) // true
-	fmt.Printf("%t\n",strings.HasSuffix(str,"str")) // false
-
+	fmt.Printf("%t\n", strings.HasSuffix(str, "ing")) // true
+	fmt.Printf("%t\n", strings.HasSuffix(str, "str")) // false
 
 	// 2. 包含关系
-	fmt.Printf("%t\n",strings.Contains(str,"an"))   // true
-	fmt.Printf("%t\n",strings.Contains(str,"test")) // false
+	fmt.Printf("%t\n", strings.Contains(str, "an"))   // true
+	fmt.Printf("%t\n", strings.Contains(str, "test")) // false
 	// 按字符查找是否包含, 字符用单引号
-	fmt.Printf("%t\n",strings.ContainsRune(str,'a')) // true
-	fmt.Printf("%t\n",strings.ContainsRune(str,99)) // false
-	fmt.Printf("%t\n",strings.ContainsRune(str,'中')) // false
-	// 多个字符是否都包含在字符串中
-	fmt.Printf("%t\n",strings.ContainsAny("chain","b")) // false
-	fmt.Printf("%t\n",strings.ContainsAny("chain","c")) // true
-	fmt.Printf("%t\n",strings.ContainsAny("chain","c & i")) // true
-
+	fmt.Printf("%t\n", strings.ContainsRune(str, 'a')) // true
+	fmt.Printf("%t\n", strings.ContainsRune(str, 99))  // false
+	fmt.Printf("%t\n", strings.ContainsRune(str, '中')) // false
+	// 判断字符串 s 中是否包含	chars 中的任一字符
+	fmt.Printf("%t\n", strings.ContainsAny("chain", "b"))   // false
+	fmt.Printf("%t\n", strings.ContainsAny("chain", " "))   // false
+	fmt.Printf("%t\n", strings.ContainsAny("ch ain", " m")) // true
+	fmt.Printf("%t\n", strings.ContainsAny("chain", "ci"))  // true
 
 	// 3. 索引位置
-	fmt.Printf("%d\n",strings.Index(str,"an"))   // 8
-	fmt.Printf("%d\n",strings.Index(str,"test")) // -1
-	fmt.Printf("%d\n",strings.LastIndex(str,"a")) // 22
+	fmt.Printf("%d\n", strings.Index(str, "an"))    // 8
+	fmt.Printf("%d\n", strings.Index(str, "test"))  // -1
+	fmt.Printf("%d\n", strings.LastIndex(str, "a")) // 22
 	// 按字符查找索引位, 字符用单引号
-	fmt.Printf("%d\n",strings.IndexRune("chain",rune('c'))) // 0
-	fmt.Printf("%d\n",strings.IndexRune("chain",'s')) // -1
-	fmt.Printf("%d\n",strings.IndexRune("i'm a 程序猿",'程')) // 6
-
+	fmt.Printf("%d\n", strings.IndexRune("chain", rune('c'))) // 0
+	fmt.Printf("%d\n", strings.IndexRune("chain", 's'))       // -1
+	fmt.Printf("%d\n", strings.IndexRune("i'm a 程序猿", '程'))   // 6
 
 	// 4. 统计次数
 	// strings.Count(s, str string) int
 	// 统计 str 在 s 中出现的次数
-	fmt.Println(strings.Count("banana","a"))  // 3
-	fmt.Println(strings.Count("banana","ba")) // 1
-	fmt.Println(strings.Count("banana","c"))  // 0
-	fmt.Println(strings.Count("apple banana"," ")) // 1
-
+	fmt.Println(strings.Count("banana", "a"))       // 3
+	fmt.Println(strings.Count("banana", "ba"))      // 1
+	fmt.Println(strings.Count("banana", "c"))       // 0
+	fmt.Println(strings.Count("apple banana", " ")) // 1
 
 	// 5. 重复字符串
 	// strings.Repeat(s, count int) string
 	// 将 s 重复 count 次并返回
-	fmt.Println(strings.Repeat("abc",2)) // abcabc
-
+	fmt.Println(strings.Repeat("abc", 2)) // abcabc
 
 	/*================================ 修改字符串相关函数 ================================*/
 
@@ -70,35 +66,30 @@ func main() {
 	fmt.Println(strings.Replace("oink oink oink", "k", "ky", 2))  // oinky oinky oink
 	fmt.Println(strings.Replace("oink oink oink", "k", "ky", -1)) // oinky oinky oinky
 
-
 	// 7. 字符串大小写
 	fmt.Println(strings.ToLower("ABC")) // abc
 	fmt.Println(strings.ToUpper("def")) // DEF
-
 
 	// 8. 修剪字符串
 	// 去掉字符串两端的空白字符
 	fmt.Println(strings.TrimSpace(" trim space ")) // "trim space"
 	// 去掉字符串两端的指定字符串
-	fmt.Println(strings.Trim("$trim string$","$")) // "trim string"
+	fmt.Println(strings.Trim("$trim string$", "$")) // "trim string"
 	// 去掉字符串左端的指定字符串
-	fmt.Println(strings.TrimLeft("## trim left ##","##"))  // " trim left ##"
+	fmt.Println(strings.TrimLeft("## trim left ##", "##")) // " trim left ##"
 	// 去掉字符串右端的指定字符串
-	fmt.Println(strings.TrimRight("## trim left ##","##")) // "## trim left"
-
+	fmt.Println(strings.TrimRight("## trim left ##", "##")) // "## trim left"
 
 	// 9. 分割字符串，将字符串转成数组
 	// 以空白符作为分割符，  \t \n \v \f \r ' ' 都是分割符
-	fmt.Printf("%q\n",strings.Fields("Hello 世\n界!\tHe\vl\flo!")) // ["Hello" "世" "界!" "He" "l" "lo!"]
+	fmt.Printf("%q\n", strings.Fields("Hello 世\n界!\tHe\vl\flo!")) // ["Hello" "世" "界!" "He" "l" "lo!"]
 	// 指定字符串为分割符
-	fmt.Printf("%q\n",strings.Split("a,b,c",",")) // [a, b, c]
-	fmt.Printf("%q\n",strings.Split(" xyz ",""))  // [" " "x" "y" "z" " "]
-	fmt.Printf("%q\n",strings.Split("","xyz"))    // [""]
-
+	fmt.Printf("%q\n", strings.Split("a,b,c", ",")) // [a, b, c]
+	fmt.Printf("%q\n", strings.Split(" xyz ", ""))  // [" " "x" "y" "z" " "]
+	fmt.Printf("%q\n", strings.Split("", "xyz"))    // [""]
 
 	// 10. 数组转字符串，按指定字符串连接
 	arr := []string{"foo", "bar", "baz"}
-	fmt.Println(strings.Join(arr,",")) // foo,bar,baz
-
+	fmt.Println(strings.Join(arr, ",")) // foo,bar,baz
 
 }
